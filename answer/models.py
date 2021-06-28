322bd0377047a8bfe2c66067927476df42e9655f
Dltmd202 <dltmd202@gmail.com>
2021-06-29 03:43:01 +0900

    임포트 내용 오타 수정

--- answer/models.py
+++ answer/models.py
@@ -1,14 +1,15 @@
 from django.db import models
-from django.contrib.auth.modles import User
-from question.modles import Question
+from django.contrib.auth.models import User
+from question.models import Question
 from user.models import CustomUser
+
 
 class Answer(models.Model):
     question_id = models.ForeignKey(Question, null=True, on_delete=models.SET_NULL)
     answer_date = models.DateTimeField(auto_now_add=True)
     user_id = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.SET_NULL)
-    vote_count= models.IntegerField(default=True)
-    is_chosen=models.BooleanField(default=False)
+    vote_count = models.IntegerField(default=True)
+    is_chosen = models.BooleanField(default=False)
     answer_title = models.CharField(null=True, max_length=100)
     answer_desc = models.TextField(null=True)
 

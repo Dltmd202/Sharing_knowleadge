33843c575cda7ebce021fdab361e0d32b743194d
Dltmd202 <dltmd202@gmail.com>
2021-06-29 03:54:55 +0900

    url 오타 수정

--- answer/views.py
+++ answer/views.py
@@ -1,27 +1,29 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.utils import timezone
-from .models import Answer, answer
+from .models import Answer
 
 
 def home(request):
     answers = Answer.objects.all()
-    return render(request, 'edit.html', {'answers':answers})
+    return render(request, 'edit.html', {'answers': answers})
+
 
 def new(request):
-    return render(request,'new.html')
+    return render(request, 'new.html')
+
 
 def create(request):
-    new_answer= Answer()
+    new_answer = Answer()
     new_answer.answer_title = request.POST['editor']
     new_answer.answer_desc = request.POST['body']
     new_answer.answer_date = timezone.now()
     new_answer.save()
     return redirect('edit', new_answer.id)
 
-def edit(request,id):
-    edit_answer = Answer.objects.get(id= id)
-    return render(request, 'edit.html', {'answer':edit_answer})
 
+def edit(request,id):
+    edit_answer = Answer.objects.get(id=id)
+    return render(request, 'edit.html', {'answer': edit_answer})
 
 
 # def detail(request,id):
--- sharing_knowleadge/urls.py
+++ sharing_knowleadge/urls.py
@@ -1,9 +1,15 @@
-from . import  home
 from django.contrib import admin
-from django.urls import path
-from . import *
+from django.urls import path, include
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', home , name="home"),
+    path('answer/', include('answer.urls')),
+    path('category/', include('category.urls')),
+    path('company/', include('company.urls')),
+    path('question/', include('question.urls')),
+    path('university/', include('university.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

a1126b38a522ea12ca760996ff3c66a12eb668b8
Dltmd202 <dltmd202@gmail.com>
2021-06-28 19:58:38 +0900

    APP들의 url과 static을 포함할 수 있도록 해주었습니다.

--- sharing_knowleadge/urls.py
+++ sharing_knowleadge/urls.py
@@ -14,8 +14,19 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('question/', include('question.urls')),
+    path('answer/', include('answer.urls')),
+    path('category/', include('category.urls')),
+    path('user/', include('user.urls')),
+    path('university', include('university.urls')),
+    path('company', include('company.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

4b6c190c4a64f16879d9af1ea396e6a779844c66
김예은 <yeheun004@gmail.com>
2021-06-28 22:49:48 +0900

    답변 작성 및 수정 기능

--- answer/views.py
+++ answer/views.py
@@ -1,3 +1,29 @@
-from django.shortcuts import render
-
+from django.shortcuts import get_object_or_404, render, redirect
+from django.utils import timezone
+from .models import Answer, answer
 # Create your views here.
+
+def edit(request):
+    answers = Answer.objects.all()
+    return render(request, 'edit.html', {'answers':answers})
+
+def new(request):
+    return render(request,'new.html')
+
+def create(request):
+    new_answer= Answer()
+    new_answer.editor = request.POST['editor']
+    new_answer.body = request.POST['body']
+    new_answer.answer_date = timezone.now()
+    new_answer.save()
+    return redirect('edit', new_answer.id)
+
+def edit(request,id):
+    edit_answer = Answer.objects.get(id= id)
+    return render(request, 'edit.html', {'answer':edit_answer})
+
+
+
+# def detail(request,id):
+#     answer = get_object_or_404(Answer,pk=id)
+#     return render(request,'detail.html',{'answer':answer})
--- sharing_knowleadge/urls.py
+++ sharing_knowleadge/urls.py
@@ -1,21 +1,12 @@
-"""sharing_knowleadge URL Configuration
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/3.2/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
+from answer.views import create, edit, home, new
 from django.contrib import admin
 from django.urls import path
+from answer.views import *
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home , name="home"),
+    path('new/', new,name="new"),
+    path('create/', create, name="create"),
+    path('edit/<str:id>', edit, name="edit")
 ]

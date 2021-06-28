f7c61e7912ec889f91eb9885f213fcddb610a30c
Dltmd202 <dltmd202@gmail.com>
2021-06-28 18:06:45 +0900

    Create Question model

--- question/models.py
+++ question/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from user.models import User
+from category.models import Category
 
-# Create your models here.
+
+class Question(models.Model):
+    user_id = models.ForeignKey(User, on_delete=models.SET_NULL)
+    category_id = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+    post_date = models.DateTimeField(auto_now_add=True)
+    modify_date = models.DateTimeField(auto_now=True)
+    ques_desc = models.TextField()
+    ques_title = models.CharField(max_length=30)

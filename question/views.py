349b3fca68f414c92fa5c248db102c1894fa9c07
Dltmd202 <dltmd202@gmail.com>
2021-06-28 21:02:21 +0900

    QuestionList와QuestionDetail의 template_name 지정

--- question/views.py
+++ question/views.py
@@ -7,12 +7,15 @@ from category.models import Category
 
 class QuestionList(ListView):
     model = Question
+    template_name = 'question/question_list.html'
     ordering = '-pk'
     paginate_by = 5
 
 
 class QuestionDetail(DetailView):
     model = Question
+    template_name = 'question/question_detail.html'
+
 
 
 class QuestionCreate():

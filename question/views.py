33e893b781260f301fcaf6dc66ff01e00c4c4183
Dltmd202 <dltmd202@gmail.com>
2021-06-28 19:37:11 +0900

    QuestionList와 QuestionDetail에 대한 view 함수정의

--- question/views.py
+++ question/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView
 
-# Create your views here.
+from .models import Question
+from category.models import Category
+
+
+class QuestionList(ListView):
+    model = Question
+    ordering = '-pk'
+    paginate_by = 5
+
+
+class QuestionDetail(DetailView):
+    model = Question
+
+
+class QuestionCreate():
+    pass
+
+
+class QuestionUpdate():
+    pass
+

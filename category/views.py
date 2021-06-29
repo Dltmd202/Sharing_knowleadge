f7ec12bdca0fd24b5de7eeaf09e56382b56845ce
Dltmd202 <dltmd202@gmail.com>
2021-06-30 04:49:37 +0900

    question_list에 카테고리를 구현

--- category/views.py
+++ category/views.py
@@ -1,3 +1,25 @@
 from django.shortcuts import render
-
+from question.models import Question
+from .models import Category
 # Create your views here.
+
+
+def category_page(request, slug):
+    if slug == 'no_category':
+        category = '미분류'
+        question_list = Question.objects.filter(category_id=None)
+    else:
+        category = Category.objects.get(slug=slug)
+        question_list = Question.objects.filter(category_id=category)
+
+    return render(
+        request,
+        'question/question_list.html',
+        {
+            'object_list': question_list,
+            'categories': Category.objects.all(),
+            'no_category_question_count': Question.objects.filter(category_id=None).count(),
+            'category': category,
+        }
+
+    )

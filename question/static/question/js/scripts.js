033430c7ec010ed06940ab4bd0a5928f7749e6cd
Dltmd202 <dltmd202@gmail.com>
2021-07-01 06:20:43 +0900

    Navbar 작업

--- question/static/question/js/scripts.js
+++ question/static/question/js/scripts.js
@@ -14,3 +14,19 @@ document.getElementById('search-input').addEventListener('keyup',
         searchQuestion()
     }
     });
+
+function navSearchQuestion() {
+    let serachValue = document.getElementById('nav-search-input').value.trim()
+    if (serachValue.length > 1) {
+        location.href = "question/search/" + serachValue +"/"
+    } else {
+        alert("검색어가 짧습니다");
+    }
+}
+
+document.getElementById('nav-search-input').addEventListener('keyup',
+    function (event){
+    if(event.key === 'Enter'){
+        searchQuestion()
+    }
+    });
--- question/urls.py
+++ question/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.QuestionList.as_view()),
+    path('', views.QuestionList.as_view(), name="question"),
     path('<int:pk>/', views.QuestionDetail.as_view(), name='res'),
     path('create_question/', views.QuestionCreate.as_view()),
     path('update_question/', views.QuestionUpdate.as_view()),

9322db90e519ef046ec58dcd8a707e6437a48613
chaeho <cogh7828@naver.com>
2021-07-01 23:40:17 +0900

    company 일부 변경

--- company/views.py
+++ company/views.py
@@ -1,8 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from company.models import Company
 from django.views.generic import CreateView
 
 class CompCreate(CreateView):
     model = Company
     fields = ['comp_name','comp_title','employ_type','join_date','leave_date','is_working']
-    template_name = "new.html"
+    template_name = "company/new.html"

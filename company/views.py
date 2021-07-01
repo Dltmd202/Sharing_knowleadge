a999a90fac5fb578d85576ec76bb96837130de6f
chaeho <cogh7828@naver.com>
2021-07-01 23:29:08 +0900

    University 일부 변경

--- company/views.py
+++ company/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from company.models import Company
+from django.views.generic import CreateView
 
-# Create your views here.
+class CompCreate(CreateView):
+    model = Company
+    fields = ['comp_name','comp_title','employ_type','join_date','leave_date','is_working']
+    template_name = "new.html"
--- sharing_knowleadge/settings.py
+++ sharing_knowleadge/settings.py
@@ -43,7 +43,9 @@ INSTALLED_APPS = [
     'category',
     'question',
     'user',
-    'home'
+    'home',
+    'university',
+    'company',
 ]
 
 MIDDLEWARE = [

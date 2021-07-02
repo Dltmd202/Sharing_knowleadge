50024918e246a15015ae196388ec2065a2e84143
Dltmd202 <dltmd202@gmail.com>
2021-07-02 15:38:15 +0900

    투표기능과 추천에 대한 view 함수와 템플릿 적용

--- answer/urls.py
+++ answer/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
     path('', views.AnswerList.as_view()),
     path('<int:pk>/', views.AnswerDetail.as_view(), name='res'),
     path('<int:pk>/new_answer', views.new_answer),
+    path('<int:pk>/vote/<int:answer_pk>/', views.vote_answer, name="vote_answer"),
     path('edit_answer/', views.AnswerEdit.as_view()),
     path('answer/search/<str:q>/', views.AnswerSearch.as_view()),
     path('answer/search/<str:q>/<str:pk>', views.AnswerDetail.as_view()),

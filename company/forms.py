b152eae28371ea6f511f9f13d8cc25cfc87efbbc
patrickkkim <patkim97@gmail.com>
2021-07-04 02:06:19 +0900

    스펙 작성 폼 스타일 수정

--- company/forms.py
+++ company/forms.py
@@ -1,12 +1,32 @@
 from django import forms
+from django.forms.widgets import CheckboxInput, TextInput
 from .models import Company
 from .widgets import DateInput
+
+classValue = "form-control px-3"
+styleValue = "background-color: wheat; height: 3rem;"
 
 class CompanyForm(forms.ModelForm):
     class Meta:
         model = Company
         fields = ['join_date', 'leave_date','comp_name', 'comp_title', 'employ_type', 'is_working']
         widgets = {
-            'join_date': DateInput(),
-            'leave_date': DateInput(),
+            'join_date': DateInput(attrs={
+                "class": classValue, "style": styleValue
+            }),
+            'leave_date': DateInput(attrs={
+                "class": classValue, "style": styleValue
+            }),
+            'comp_name': TextInput(attrs={
+                "placeholder": "카카오커머스", "class": classValue, "style": styleValue
+            }),
+            'comp_title': TextInput(attrs={
+                "placeholder": "백엔드 개발자", "class": classValue, "style": styleValue
+            }),
+            'employ_type': TextInput(attrs={
+                "placeholder": "정규직", "class": classValue, "style": styleValue
+            }),
+            'is_working': CheckboxInput(attrs={
+                "class": "form-check-input", "id":"isWorkingCheckBox"
+            }),
         }
--- university/forms.py
+++ university/forms.py
@@ -1,12 +1,32 @@
 from django import forms
 from .models import University
 from .widgets import DateInput
+from django.forms.widgets import CheckboxInput, TextInput
+
+classValue = "form-control px-3"
+styleValue = "background-color: wheat; height: 3rem;"
 
 class UniversityForm(forms.ModelForm):
     class Meta:
         model = University
         fields = ['enter_date', 'grad_date','uni_name', 'uni_degree', 'uni_major', 'is_attending']
         widgets = {
-            'enter_date': DateInput(),
-            'grad_date': DateInput(),
+            'enter_date': DateInput(attrs={
+                "class": classValue, "style": styleValue
+            }),
+            'grad_date': DateInput(attrs={
+                "class": classValue, "style": styleValue
+            }),
+            'uni_name': TextInput(attrs={
+                "placeholder": "서울대학교", "class": classValue, "style": styleValue
+            }),
+            'uni_degree': TextInput(attrs={
+                "placeholder": "석사", "class": classValue, "style": styleValue
+            }),
+            'uni_major': TextInput(attrs={
+                "placeholder": "컴퓨터공학과", "class": classValue, "style": styleValue
+            }),
+            'is_attending': CheckboxInput(attrs={
+                "class": "form-check-input", "id":"isAttendingCheckBox"
+            }),
         }

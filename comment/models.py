7f834f85a1ce6c3ad309f0c11e4b34ce2ec0f63a
김예은 <yeheun004@gmail.com>
2021-08-09 13:30:20 +0900

    comment 수정

--- comment/models.py
+++ comment/models.py
@@ -1,14 +1,15 @@
 from django.db import models
 from answer.models import Answer
+from user.models import CustomUser
 
 class Comment(models.Model):
     answer_id = models.ForeignKey(Answer, null=True, on_delete=models.SET_NULL)
-    comment_date = models.DateTimeField(auto_now_add=True, null=True)
-    modified_date = models.DateTimeField(auto_now_add=True, null=True)
-    comment_desc = models.TextField(null=True)
+    comment_date = models.DateTimeField(auto_now_add=True)
+    user_id = models.ForeignKey(CustomUser, null=True, blank=True, on_delete=models.SET_NULL)
+    comment_desc = models.TextField()
 
     def __str__(self):
-        return str(self.pk) + ': ' + self.answer_id
+        return str(self.comment_desc)
 
     def get_absolute_url(self):
-        return f'/question/{self.pk}/'
+        return f'/comment/{self.pk}/'

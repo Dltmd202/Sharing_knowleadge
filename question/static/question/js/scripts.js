dc8b7b50009e6421213cd5a60411e9f2ba04e306
Dltmd202 <dltmd202@gmail.com>
2021-08-09 11:06:59 +0900

    비로그인 상태에서 질문 포인트를 질의하는 js 이슈 해결

--- question/static/question/js/scripts.js
+++ question/static/question/js/scripts.js
@@ -1,23 +1,34 @@
 let quesPoint;
 let inputPoint;
-let innerPoint
+let innerPoint;
+let questionDetail;
+let questionArticle;
 
 window.onload = function () {
     quesPoint = document.querySelector("#ques_point");
     inputPoint = document.querySelector("#question__input__point");
-    innerPoint = document.querySelector("#ques_point").innerText;
+    innerPoint = document.querySelector("#ques_point");
+
+    questionDetail = document.getElementsByClassName("question__form");
+    questionArticle = document.getElementsByClassName("question__article");
+    console.log(questionDetail[0]);
+    console.log(questionArticle[0]);
+
+    
     console.log(document.querySelector("#question__input__point"));
-    console.log(inputPoint.innerText);
-    document.querySelector("#question__input__point").addEventListener('keyup',
-        function(event){
-            console.log(event);
-            console.log(inputPoint.value);
-            console.log(innerPoint);
-            console.log(inputPoint.value + innerPoint);
-            if (parseInt(inputPoint.value) > parseInt(innerPoint)){
-                alert("질문 포인트가 부족합니다.");
-                inputPoint.value = parseInt(innerPoint);
+
+    if(inputPoint){
+        inputPoint.addEventListener('keyup',
+            function(event){
+                console.log(event);
+                console.log(inputPoint.value);
+                console.log(innerPoint.innerText);
+                console.log(inputPoint.value + innerPoint);
+                if (parseInt(inputPoint.value) > parseInt(innerPoint.innerText)){
+                    alert("질문 포인트가 부족합니다.");
+                    inputPoint.value = parseInt(innerPoint.innerText);
+                }
             }
-        }
-        )
+        );
+    }
 }
